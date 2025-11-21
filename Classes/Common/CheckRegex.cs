f3b76087d06a5b9c64328e778cf95313b5903eec
Shahzod0079 <toshmatovshahzod69@gmail.com>
2025-11-21 11:14:14 +0500

    Создание классов. Тошматов

---
+++ Classes/Common/CheckRegex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regexx_Toshmatov.Classes.Common
+{
+    public class CheckRegex
+    {
+        /// <summary>
+        /// Метод поиска совпадений
+        /// </summary>
+        /// <param name="Pattern">Регулярное выражение</param>
+        /// <param name="Input">Строка ввода</param>
+        /// <returns></returns>
+        public static bool Match(string Pattern, string Input)
+        {
+            Match m = Regex.Match(Input, Pattern);
+            return m.Success;
+        }
+    }
+}
---
+++ Classes/Passport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regexx_Toshmatov.Classes
+{
+    public class Passport
+    {
+        public string Name { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Issued { get; set; }
+
+        public string DateOfIssued { get; set; }
+
+        public string DepartmentCode { get; set; }
+
+        public string SeriesAndNumber { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        public string PlaceOfBirth { get; set; }
+    }
+}
